c6b0a185b10cbc0eb472c941a2805d62e02d0d91
NegiAbhijeet <abhijeetnegi.ab@gmail.com>
2025-08-28 11:47:29 +0530

    profile change

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,20 +1,21 @@
 // src/components/layout/TopBar.tsx
 import React from "react";
-import { Image, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const TopBar = () => {
   return (
     <View className="flex-row justify-between items-center h-14 px-6 py-2 bg-white border-b border-black/10">
-      <Image
+      <View className="flex flex-row items-center  gap-2"><Image
         source={require("@/assets/logo.png")}
         className="h-10 w-10"
         resizeMode="contain"
       />
-      <TouchableOpacity>
+      <Text className="font-semibold text-lg">AI Interviewagents</Text></View>
+      <TouchableOpacity className="border-2 border-gray-500 rounded-full p-2">
         <Image
           source={require("@/assets/profile.png")}
-          className="w-10 h-10"
-          resizeMode="contain"
+          className="w-6 h-6"
+          resizeMode="cover"
         />
       </TouchableOpacity>
     </View>
